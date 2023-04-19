f54c7173e6e38114507e27ce2b86b256fe853e0b
Максим Литвин / Maksym Lytvyn <104524765+Maksym-Lytvyn@users.noreply.github.com>
2023-04-19 21:11:18 +0300

    up

--- src/index.js
+++ src/index.js
@@ -9,17 +9,24 @@ const submit = document.querySelector('.submit-btn');
 console.log(submit);
 const gallery = document.querySelector('.gallery');
 console.log(gallery);
-const button = document.querySelector('.button');
-console.log(button);
+const loadMore = document.querySelector('.load-btn');
+console.log(loadMore);
+const buttonArea = document.querySelector('.button-area');
+console.log(buttonArea);
 
+// визначаємо початкові "лічильні" змінні
 let value;
+let page = 1;
+let perPage = 20;
+
+
 // виводимо рядок запиту з поля в функцію 
 input.addEventListener('input', (event) => {
     value = event.target.value;
     console.log(value);
     if (value.length < 1){
         gallery.innerHTML = '';
-        button.innerHTML = '';
+        loadMore.classList.add('hidden');
         Notiflix.Notify.warning('Результати пошуку скинуто')
     }
 })
@@ -29,7 +36,7 @@ input.addEventListener('input', (event) => {
 submit.addEventListener('click', (event) => {
     event.preventDefault();
     console.log(value);
-    fetch(`https://pixabay.com/api/?key=35499078-ae1aac6b87ed3c45ca8fde2a7&q=${value}&image_type=photo&orientation=horizontal&safesearch=true`)
+    fetch(`https://pixabay.com/api/?key=35499078-ae1aac6b87ed3c45ca8fde2a7&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}`)
     .then(response => response.json())
     .then(pictures => {
         if (pictures.total === 0) {
@@ -39,6 +46,7 @@ submit.addEventListener('click', (event) => {
             Notiflix.Notify.success(`Пошук здійснено! Знайдено ${pictures.totalHits} зображень`);
             console.log(pictures);
             renderImagesGallery(pictures);
+            loadMore.classList.remove('hidden');
         }
         }
         )
@@ -47,6 +55,32 @@ submit.addEventListener('click', (event) => {
     
 })
 
+
+// робимо ідентичний запит, але розширюючи об'єм результатів з кожним натисканням
+loadMore.addEventListener('click', () => {
+  perPage +=20
+  fetch(`https://pixabay.com/api/?key=35499078-ae1aac6b87ed3c45ca8fde2a7&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=${perPage}`)
+    .then(response => response.json())
+    .then(pictures => {
+        if (pictures.total === 0) {
+            Notiflix.Notify.failure('Вибачте, але за вашим пошуковим запитом не знайдено зображень. Спробуйте ще раз')
+        }
+        else if (pictures.hits.length === pictures.totalHits){
+          Notiflix.Notify.failure('Вибачте, але ліміт запитів вичерпано');
+        }
+        else {
+            console.log(pictures);
+            renderImagesGallery(pictures);
+            loadMore.classList.remove('hidden');
+        }
+        }
+        )
+    
+    .catch(error => console.log(error));
+})
+
+
+// виводимо потрібні дані з масиву об'єктів та розкладаємо їх по тегах в шаблонній розмітці
 function renderImagesGallery(pictures){
    gallery.innerHTML = pictures.hits.map((hit) => {
     return `
@@ -79,12 +113,11 @@ function renderImagesGallery(pictures){
   </div>
   `
    })
-   button.innerHTML = `<button class='load-btn'>Завантажити ще</button>`;
 }
 
 
 
-
+// Початкове сповіщення застосунку
 function Notify() {
     Notiflix.Notify.info('Програма готова!');
 }
